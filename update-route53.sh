dece1c72dd19edcf1e9a8eb569f722e8d6c776b4
markosluga <35494371+markosluga@users.noreply.github.com>
2023-01-16 12:27:41 -0500

    Update update-route53.sh

--- update-route53.sh
+++ update-route53.sh
@@ -1,3 +1,11 @@
+#Get the public IP and resolve the DNS name - replace the "replace.withyour.fqdn" address with the hostname you are updating
 IP=$(curl checkip.amazonaws.com)
+DNS=$(dig +short replace.withyour.fqdn)
+#compare the results - if they match, exit. If they do exit, if they don't update the DNS.
+if [[ "$IP" == "$DNS" ]];
+then
+        exit
+else
 jq '.Changes[].ResourceRecordSet.ResourceRecords[].Value="'$IP'"' r53record.json > updateroute53.json
 aws route53 change-resource-record-sets --hosted-zone-id YOURZONEIDGOESHERE --change-batch file://updateroute53.json
+fi
